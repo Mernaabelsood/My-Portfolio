4992dada5a2d9b7310cf7fcc0786f1998f3dae16
Mernaabelsood <merna217@gmail.com>
2025-03-16 10:06:17 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import AppWrapper from "./components/AppWrapper"; // Import the wrapper
+import AppWrapper from "./components/AppWrapper";
 
 function App() {
   return (
--- src/components/AppWrapper.jsx
+++ src/components/AppWrapper.jsx
@@ -1,14 +1,11 @@
-import React from "react";
-
 // const AppWrapper = ({ children }) => (
 //   <div className="bg-gradient-to-r from-pink-200 to-purple-300 min-h-screen w-full">
 //     {children}
 //   </div>
 // );
 
-
 const AppWrapper = ({ children }) => (
-  <div className="bg-gradient-to-r from-[#F8FAFC] to-[#D9EAFD] min-h-screen w-full">
+  <div className="bg-gradient-to-r from-[#F8FAFC] to-[#D9EAFD] min-h-screen w-full flex flex-col items-center justify-center px-4 sm:px-6 md:px-8 overflow-x-hidden">
     {children}
   </div>
 );
