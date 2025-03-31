5383d813b73a46b34a06a2d6f11be97f79b8cee9
Mernaabelsood <merna217@gmail.com>
2025-03-31 20:39:01 +0200

    updates

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,18 +7,21 @@ import image4 from "../assests/images/image4.png";
 import image5 from "../assests/images/image5.png";
 import image6 from "../assests/images/image6.png";
 import image7 from "../assests/images/image7.png";
+import image8 from "../assests/images/image8.png";
+
 
 const projects = [
 
   {
-    name: "E-commerce Application",
+    name: "Wild Oasis Website",
     description:
-      "A Responsive React Web App Featuring A Dynamic Product Catalog, Detailed Product Pages, Shopping Cart, Wishlist, And Checkout Functionality. It Offers Easy Navigation.",
-    techStack: "Context API, React Query, React Router, Bootstrap",
-    liveDemo: "https://e-commerce-project-sepia-alpha.vercel.app/",
-    github: "https://github.com/Mernaabelsood/E-commerce-project",
-    image: image1, 
+      "A fullstack website for discovering and booking cabins, featuring secure authentication, a responsive UI, and server-side rendering (SSR) for optimal performance and SEO.",
+    techStack: "Next.Js, React.Js, Context API, Supabase, Tailwind CSS",
+    liveDemo: "https://wild-oasis-website-jade.vercel.app/",
+    github: "https://github.com/Mernaabelsood/Wild-Oasis-Website",
+    image: image8, 
   },
+
   {
     name: "Pizza Store",
     description:
@@ -27,6 +30,15 @@ const projects = [
     liveDemo: "https://pizza-store-eight.vercel.app/",
     github: "https://github.com/Mernaabelsood/pizza-store",
     image: image2,
+  },
+  {
+    name: "E-commerce Application",
+    description:
+      "A Responsive React Web App Featuring A Dynamic Product Catalog, Detailed Product Pages, Shopping Cart, Wishlist, And Checkout Functionality. It Offers Easy Navigation.",
+    techStack: "Context API, React Query, React Router, Bootstrap",
+    liveDemo: "https://e-commerce-project-sepia-alpha.vercel.app/",
+    github: "https://github.com/Mernaabelsood/E-commerce-project",
+    image: image1, 
   },
   {
     name: " Social Media App",
@@ -41,7 +53,7 @@ const projects = [
     name: "Wild Oasis Application",
     description:
       "A modern React cabin booking web app featuring real-time data fetching, sorting, filtering, dark mode, pagination and many features.",
-    techStack: "React Router, React Query, Styled Components",
+    techStack: "React Router, supabase, React Query, Styled Components",
     liveDemo: "https://the-wild-oasis-lovat-eight.vercel.app/",
     github: "https://github.com/Mernaabelsood/The-Wild-Oasis",
     image: image7, 
